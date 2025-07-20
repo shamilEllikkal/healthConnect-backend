086b2f5bbecf55c6f167180a49f3bf44aa3681bd
shamilEllikkal <shamilellikkal123@gmail.com>
2025-07-21 01:39:15 +0530

    cors issue resolved

--- server.js
+++ server.js
@@ -16,7 +16,11 @@ const PORT = process.env.PORT;
 
 
 app.use(express.json());
-app.use(cors({ origin: 'http://localhost:5173', credentials: true }));
+app.use(cors({
+  origin: ['http://localhost:5173', 'https://healthconnectbookingapp.netlify.app'],
+  credentials: true,
+}));
+
 
 app.use("/api",Routers)
 
